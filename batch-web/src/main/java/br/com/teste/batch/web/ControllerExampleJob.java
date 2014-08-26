69035dd9ff0cac4af90139b02431c5b32a0b88f9
carlosrgomes@gmail.com <carlosrgomes@gmail.com@a02a9370-652c-2e37-0122-88c918cb2873>
2014-08-26 16:49:57 +0000

    SDD-237

--- batch-web/src/main/java/br/com/teste/batch/web/ControllerExampleJob.java
+++ batch-web/src/main/java/br/com/teste/batch/web/ControllerExampleJob.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("run")
-public class ControllerExampleJob {
+public class ControllerExampleJob    {
 	
 	@Autowired
 	private JobLauncher jobLauncher;
